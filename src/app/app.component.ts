1b00e053ca2f84e758b4a4668e224ed21b6ca292
Fede <biefrost21@gmail.com>
2020-08-17 20:58:15 -0300

    Proyecto Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipes';
+ 
+  nombre = 'Federico';
+  
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre : "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa : "19"
+    }
+
+  }
 }
